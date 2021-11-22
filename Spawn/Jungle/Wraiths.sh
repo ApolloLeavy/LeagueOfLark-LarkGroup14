13c1afabbe1aa40d9d48dc818356d6325e490101
apollo leavy <aleavy8732@floridapoly.edu>
2021-11-21 22:11:16 -0500

    Wraiths

--- Spawn/Jungle/Wraiths.sh
+++ Spawn/Jungle/Wraiths.sh
@@ -1,1 +1,17 @@
-
+#!/bin/bash
+HP=$((20+7*$level))
+AD=$((8+3*$level))
+echo Wraiths HP:"$HP" AD:"$AD"
+while [ $HP -gt 0 ]
+do
+read action
+if [ $action == "attack" ]
+then
+        HP=$(($HP-$ad))
+        echo Wraiths HP:"$HP"
+        health=$(($health-$AD))
+        echo Your HP:  "$health"
+fi
+done
+experience=$(($experience+55+4*$level))
+. exp.sh
